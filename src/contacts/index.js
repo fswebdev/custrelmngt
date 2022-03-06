b95652bdb44544e7ce87e86d03e6ffc35b6de777
fswebdev <47744494+fswebdev@users.noreply.github.com>
2022-03-07 00:06:19 +0530

    Create index.js

---
+++ src/contacts/index.js
@@ -0,0 +1,11 @@
+import { ContactShow } from './ContactShow';
+import { ContactList } from './ContactList';
+import { ContactEdit } from './ContactEdit';
+import { ContactCreate } from './ContactCreate';
+
+export default {
+    list: ContactList,
+    show: ContactShow,
+    edit: ContactEdit,
+    create: ContactCreate,
+};
