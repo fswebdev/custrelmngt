e23337819ca95e4a2dc376b60c5fa7f9a20c3c44
fswebdev <47744494+fswebdev@users.noreply.github.com>
2022-03-06 21:24:00 +0530

    Create build.js

---
+++ build.js
@@ -0,0 +1,9 @@
+const rewire = require('rewire');
+const defaults = rewire('react-scripts/scripts/build.js');
+const config = defaults.__get__('config');
+
+config.optimization.minimizer[0].options.terserOptions.keep_classnames = true;
+config.optimization.minimizer[0].options.terserOptions.keep_fnames = true;
+
+config.resolve.alias['react-dom$'] = 'react-dom/profiling';
+config.resolve.alias['scheduler/tracing'] = 'scheduler/tracing-profiling';
