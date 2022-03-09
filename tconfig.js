d26c2accc85b8e14212cfa4d84bd5869d0a66782
fswebdev <47744494+fswebdev@users.noreply.github.com>
2022-03-09 17:12:51 +0530

    Update tconfig.js

--- tconfig.js
+++ tconfig.js
@@ -22,5 +22,16 @@
   },
   "include": [
     "src"
-  ]
+  ],
+    webpack.config.js:
+
+{ 
+    test: /\.jsx?$/,         // Match both .js and .jsx files
+    exclude: /node_modules/, 
+    loader: "babel", 
+    query:
+      {
+        presets:['react']
+      }
+}
 }
